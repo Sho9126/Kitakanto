4165c84e08823dd6f317fcae483eea30eca24c04
Sho9126 <149132441+Sho9126@users.noreply.github.com>
2023-11-02 14:49:33 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = () =>{
+    setTimeout(classAdd, 2000)
+}
+
+const classAdd = () =>{
+    const snipper = document.getElementById('loading');
+    snipper.classList.add('loadead');
+}
